325d7b201cfd6a456bafc9205c616846c226f938
nsagyntay <sagyntajnazerke@gmail.com>
2025-03-11 15:44:51 +0500

    add changes for 1.py

--- lab_7/1.py
+++ lab_7/1.py
@@ -1,44 +1,56 @@
 import pygame
 import time
+import math
+from datetime import datetime
 
 pygame.init()
+w = 600
+h = 600
+c = (w // 2, h// 2)
+back_color = (255 ,255 ,255)
 
-screen = pygame.display.set_mode((800, 700))
+screen = pygame.display.set_mode((w , h ))
 pygame.display.set_caption("Mickey Clock")
+clock = pygame.time.Clock()
 
 back = pygame.image.load(r"C:\Users\User\OneDrive\Рабочий стол\ns_vs\lab_7\mickeyclock.png")
-back = pygame.transform.scale(back, (800, 700))
-l_hand = pygame.image.load(r"C:\Users\User\OneDrive\Рабочий стол\ns_vs\lab_7\right_hand.png")  
 r_hand = pygame.image.load(r"C:\Users\User\OneDrive\Рабочий стол\ns_vs\lab_7\left_hand.png")
+l_hand = pygame.image.load(r"C:\Users\User\OneDrive\Рабочий стол\ns_vs\lab_7\right_hand.png")
 
-def draw_hand(image, angle, x, y):
-    rotated = pygame.transform.rotate(image, angle)
-    rect = rotated.get_rect(center=(400, 350))
-    screen.blit(rotated, rect.topleft)
+
+back = pygame.transform.scale(back,(w,h))
+r_hand = pygame.transform.scale(r_hand, (255,100))
+l_hand = pygame.transform.scale(l_hand, (255, 100))
+
+
+r_hand_offset = (0,0)
+l_hand_offset = (0,0)
+
+def rot_center(image, angle, offset):
+    rotated_image = pygame.transform.rotate(image, angle)
+    rect = rotated_image.get_rect(center=(c[0] + offset[0], c[1] + offset[1]))
+    screen.blit(rotated_image, rect.topleft)
 
 running = True
 while running:
-    screen.fill((255, 255, 255))
+    screen.fill(back_color)
     screen.blit(back, (0, 0))
 
-    cur_t = time.localtime()
-    min = cur_t.tm_min
-    sec = cur_t.tm_sec
+    now = datetime.now()
+    min = now.minute
+    sec = now.second
+    
+    min_angle = -(min % 60) * 6 
+    sec_angle = -(sec % 60) * 6
 
-    sec_a = -sec * 6
-    min_a = -min * 6
-
-    draw_hand(l_hand, sec_a, 400, 350)
-    draw_hand(r_hand, min_a, 400, 350)
+    rot_center(r_hand,min_angle, r_hand_offset)
+    rot_center( l_hand, sec_angle, l_hand_offset)        
 
     pygame.display.flip()
+    clock.tick(1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-
-    pygame.time.delay(1000)
 
 pygame.quit()
